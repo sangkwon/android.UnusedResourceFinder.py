dac65423656e5632a6f5ea55047609bc7ccff261
sangkwon <sangkwon.k@gmail.com>
2013-08-26 00:33:20 +0900

    implements

--- findres.py
+++ findres.py
@@ -1,4 +1,59 @@
+#!/usr/bin/python
 #findres.py
+
 import os
+import sys
+import glob
+import fileinput
+
+#### Function Define ##########
+def findFirstUser(path,drawableName):
+	for (root,dirs,files) in os.walk(path):
+		for file in files:
+			filePath = os.path.join(root,file)
+			ext = os.path.splitext(file)[-1]
+			used = None
+			if ext==".java":
+				used = findInFile(filePath, "R.drawable."+drawableName)
+			elif ext==".xml":
+				used = findInFile(filePath, "@drawable/"+drawableName)
+
+			if used:
+				return filePath
+
+	return None
 
 
+def findInFile(file,str):
+	f=open(file)
+	content=f.read()
+	f.close()
+	return content.find(str) >= 0
+    
+
+
+#### Main Loop ###############
+
+if len(sys.argv)<=1:
+	print("Usages:")
+	print("\t"+__file__+"<android project path>")
+	sys.exit(-1)
+
+rootPath=sys.argv[1]
+print("RootPath is "+rootPath)
+
+# Walk
+totalLength = 0
+for drawable in glob.glob(rootPath + "/res/drawable*"):
+	print("\n### Start "+drawable)
+
+	for pngPath in glob.glob(drawable+"/*.png"):
+		pngName = os.path.splitext(os.path.basename(pngPath))[0]
+		user = findFirstUser(rootPath,pngName)
+		if not user:
+			length = os.path.getsize(pngPath)
+			print(str(length) + "\t" + pngPath)
+			totalLength+=length
+
+print("totalLength is "+str(totalLength))
+	
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 findres.py ~/Documents/workspace/android.daybe
